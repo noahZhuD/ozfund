2a633b7a812a726640441b2a78c50d9fd206da99
noahZhu <noah.zhu@austintechno.com>
2022-09-02 16:56:51 +0800

    余额对接 toto余额对接

--- ozfund/js/index.js
+++ ozfund/js/index.js
@@ -40,11 +40,22 @@ function scrollChange(_this, time = 3) {
   })
 }
 
+const usdtContractAddress = "0xd92e713d051c37ebb2561803a3b5fbabc4962431";
+const ozcoinContractAddress = "0x0eaf4e0a126d8cf1faca4c301174175a589b1cea";
+const totoContractAddress = "0xf7489804019c767086fa3c11cdf58dc14715e7ba";
+var erc20ContractAbi = null;
+var ozcoinExpandAbi = null;
+var totoExpandAbi = null;
+var ozcoinStakeExpandAbi = null;
+var web3 = ethereum ? new Web3(ethereum) : null;
+var balance = 0;
+var address = null;
+
 function setToken (key, value) {
   localStorage.setItem(key, value)
 }
 function getToken (key) {
-  localStorage.getItem(key)
+  return localStorage.getItem(key)
 }
 function removeToken (key) {
   localStorage.removeItem(key)
@@ -52,44 +63,99 @@ function removeToken (key) {
 // 是否已经链接钱包
 function linkBtnStatus(v) {
   if (v === 'success') {
-    $('.line_wallet').text('已连接').attr('disabled', true)
+    // $('.line_wallet').text('已连接').attr('disabled',  true)
+    $('.line_wallet').text('已连接').attr('disabled', false)
   } else {
     $('.line_wallet').text('连接钱包').attr('disabled', false)
   }
 }
 
-if (window.ethereum) {
-  let addr = window.ethereum.request({ method: 'eth_requestAccounts' })
-  addr.then(res => {
-    console.log(res, '已经链接啦')
+initAbi()
+$(document).ready(async function(){ 
+  if (window.ethereum) {
+    ethereumInit()
+  } else {
+    console.error('没有安装钱包插件')
+  }
+});
+
+async function ethereumInit() {
+  if (getToken('META_MASK')) {
+    startConnect()
+    return
+  }
+  // var isConnectedB = ethereum.isConnected();
+}
+async function startConnect() {
+  console.log(1)
+  let addr = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  console.log(addr, 'addr')
+  if (addr) { // 
+    setToken('META_MASK', addr[0])
+    address = addr[0]
     linkBtnStatus('success')
-    setToken('META_MASK', res[0])
-  }).catch(error => {
+    getBalance(getToken('META_MASK'))
+    getTotoBalance()
+  } else {
     linkBtnStatus('')
     removeToken('META_MASK')
-    console.log(error, '没有链接')
-  })
+  }
+}
+// 获取余额
+async function getBalance(account) {
+  let balanceHex = await ethereum.request({ method: 'eth_getBalance', params: [account,"latest"] });
+  balance = web3.utils.fromWei(String(BigInt(balanceHex)),'ether');
+}
+// 获取toto
+function getTotoBalance() {
+  getERC20Balance(totoContractAddress, $('.toto_number'), 'ether')
+  // getOkcoinStake()
+}
+
+// 获取ERC余额
+async function getERC20Balance(contractAddress, dom, unit) {
+  let myContract = new web3.eth.Contract(erc20ContractAbi, contractAddress, {
+      from: address, // default from address
+  });
+  console.log(myContract.methods.balanceOf(address), 'myContract')
+  myContract.methods.balanceOf(address).call({from: address}, function(error, result){
+    console.log(error, result, 'error result')
+      if(!error) {
+          let ubalance = web3.utils.fromWei(String(result), unit);
+          dom.html(ubalance)
+      } else {
+          console.log(error);
+      }
+  });
+}
+      
+        
+// 断开链接
+function endConnect() {
+  ethereum.on('disconnect', handler = (error) => {
+    console.log(error, '断开链接')
+  });
 }
   
 
-// 链接钱包 
+// 链接钱包按钮
 $('.line_wallet').click(function(){
   var userAgent = navigator.userAgent;
   if (!userAgent.includes('Chrome')) return alert('请使用Google浏览器')
   if (typeof window.ethereum !== 'undefined') {
-    let addr = window.ethereum.request({ method: 'eth_requestAccounts' })
-    addr.then(res => {
-      // 此处已连接
-      console.log(res, 'rrr')
-    }).catch(err => {
-      if (err.code === -32002) {
-        alert('请在MetaMask绑定用户')
-        window.open('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#initialize/welcome')
+    startConnect()
+    // let addr = window.ethereum.request({ method: 'eth_requestAccounts' })
+    // addr.then(res => {
+    //   // 此处已连接
+    //   console.log(res, 'rrr')
+    // }).catch(err => {
+    //   if (err.code === -32002) {
+    //     alert('请在MetaMask绑定用户')
+    //     window.open('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#initialize/welcome')
 
-      }
-      console.log(err, 'err')
-    })
-    
+    //   }
+    //   console.log(err, 'err')
+    // })
   } else {
     alert('没有安装钱包插件,请安装,安装后请刷新本页面再使用')
     window.open('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/home.html#initialize/welcome')
@@ -105,10 +171,65 @@ $('.line_wallet').click(function(){
 
 if (window.ethereum) {
   ethereum.on('accountsChanged', function(e) {
-    console.log(1, e)
+    console.log(1, e, '112323')
   })
   ethereum.on('chainChanged', function(e) {
     console.log(2, e)
   })
 }
 
+// 请求文件
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onload = function () {
+          if (this.status >= 200 && this.status < 300) {
+              resolve(xhr.response);
+          } else {
+              reject({
+                  status: this.status,
+                  statusText: xhr.statusText
+              });
+          }
+      };
+      xhr.onerror = function () {
+          reject({
+              status: this.status,
+              statusText: xhr.statusText
+          });
+      };
+      xhr.send();
+  });
+}
+
+// 初始化abi
+async function initAbi() {
+  jsonStr = await makeRequest("GET", "/ozfund/json/erc20Abi.json");
+  erc20ContractAbi = JSON.parse(jsonStr);
+  jsonStr = await makeRequest("GET", "/ozfund/json/ozcoinExpandAbi.json");
+  ozcoinExpandAbi = JSON.parse(jsonStr);
+  jsonStr = await makeRequest("GET", "/ozfund/json/totoExpandAbi.json");
+  totoExpandAbi = JSON.parse(jsonStr);
+  jsonStr = await makeRequest("GET", "/ozfund/json/ozcoinStakeExpandAbi.json");
+  ozcoinStakeExpandAbi = JSON.parse(jsonStr);
+}
+async function getOkcoinStake() {
+  maxFeePerGas = web3.utils.toWei( '1.5' , 'gwei' );
+  maxPriorityFeePerGas = web3.utils.toWei( '1.5' , 'gwei' );
+  gasLimit = 8000000;
+  console.log(totoExpandAbi, 'totoExpandAbi');
+  let myContract = new web3.eth.Contract(totoExpandAbi, totoContractAddress, {
+      from: address, // default from address
+      gasLimit: 70000,
+      gasPrice: 1000000000 // default gas price in wei, 10 gwei in this case
+  });
+  myContract.methods.ozcoinStake().call({from: address}, function(error, result){
+      if(!error) {
+          ozcoinStakeContractAddress = String(result)
+          console.log(ozcoinStakeContractAddress, "ozcoinStakeContractAddress")
+      } else {
+          console.log(error);
+      }
+  });
+}
